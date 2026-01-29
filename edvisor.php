f7da245efcee3eb63632f62bac4a2e4f039866c2
Murilo Azevedo <muriloazevedo338@gmail.com>
2026-01-29 18:24:11 -0300

    use env var

--- edvisor.php
+++ edvisor.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 header('Content-Type: application/json; charset=utf-8');
 
-$apiKey = "";
+$apiKey = $_SERVER['EDVISOR_API_KEY'];
 $input['agencyId'] = 2729;
 $url = 'https://api-v2.edvisor.io/graphql';
 
